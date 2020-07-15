dc2e9d9d3706d3c0ab5337fa769e0da28086f864
InumberX <inumberx@gmaile.com>
2020-07-15 21:15:44 +0900

    修正ファイルコミット

---
+++ nuxt/afterWorks_v.4.0.0/components/AdsenseList.vue
@@ -0,0 +1,38 @@
+<template>
+ <div class="ads-list-box">
+  <div class="inner">
+   <div class="ads-list">
+    <ul>
+     <li v-for="(item, i) in ads">
+      <adsense
+       :ad-slot="item.slot"
+       :ad-responsive="item.responsive"
+       :ad-format="item.format"
+      />
+     </li>
+    </ul>
+   </div>
+  </div>
+ </div>
+</template>
+
+<script>
+import Adsense from "~/components/Adsense.vue";
+
+export default {
+ components: {
+  Adsense: Adsense
+ },
+ data: function() {
+  return {
+   ads: [
+    {
+     slot: "7889882209",
+     responsive: "false",
+     format: "auto"
+    }
+   ]
+  };
+ }
+};
+</script>
--- nuxt/afterWorks_v.4.0.0/pages/index.vue
+++ nuxt/afterWorks_v.4.0.0/pages/index.vue
@@ -1,136 +1,121 @@
 <template>
-<div class="index-wrap" v-if="!this.$store.state.isLoading">
+ <div class="index-wrap" v-if="!this.$store.state.isLoading">
+  <mainVisual></mainVisual>
 
-<mainVisual></mainVisual>
+  <contentsLead v-bind:text="response.contents.indexMessage[0]"> </contentsLead>
 
-<contentsLead
- v-bind:text="response.contents.indexMessage[0]"
->
-</contentsLead>
+  <section>
+   <contentsTitle titleEn="Service" title="提供サービス"> </contentsTitle>
 
-<section>
+   <cardList
+    v-bind:titles="response.contents.indexServiceTitle"
+    v-bind:texts="response.contents.indexServiceText"
+    v-bind:classes="response.contents.indexServiceClass"
+   >
+   </cardList>
+  </section>
 
-<contentsTitle
- titleEn="Service"
- title="提供サービス"
->
-</contentsTitle>
+  <section>
+   <contentsTitle titleEn="Contents" title="コンテンツ"> </contentsTitle>
 
-<cardList
- v-bind:titles="response.contents.indexServiceTitle"
- v-bind:texts="response.contents.indexServiceText"
- v-bind:classes="response.contents.indexServiceClass"
->
-</cardList>
+   <cardList
+    v-bind:titles="response.contents.indexContentsTitle"
+    v-bind:texts="response.contents.indexContentsText"
+    v-bind:classes="response.contents.indexContentsClass"
+    v-bind:links="response.contents.indexContentsLink"
+    v-bind:linkTexts="response.contents.indexContentsLinkText"
+   >
+   </cardList>
+  </section>
 
-</section>
+  <contact></contact>
 
-<section>
-
-<contentsTitle
- titleEn="Contents"
- title="コンテンツ"
->
-</contentsTitle>
-
-<cardList
- v-bind:titles="response.contents.indexContentsTitle"
- v-bind:texts="response.contents.indexContentsText"
- v-bind:classes="response.contents.indexContentsClass"
- v-bind:links="response.contents.indexContentsLink"
- v-bind:linkTexts="response.contents.indexContentsLinkText"
->
-</cardList>
-
-</section>
-
-<contact></contact>
-
-</div>
+  <section>
+   <AdsenseList></AdsenseList>
+  </section>
+ </div>
 </template>
 
 <script>
-import MainVisual from '~/components/MainVisual.vue'
-import ContentsLead from '~/components/ContentsLead.vue'
-import ContentsTitle from '~/components/ContentsTitle.vue'
-import CardList from '~/components/CardList.vue'
-import Contact from '~/components/Contact.vue'
+import MainVisual from "~/components/MainVisual.vue";
+import ContentsLead from "~/components/ContentsLead.vue";
+import ContentsTitle from "~/components/ContentsTitle.vue";
+import CardList from "~/components/CardList.vue";
+import Contact from "~/components/Contact.vue";
+import AdsenseList from "~/components/AdsenseList.vue";
 
 export default {
  head: {
-  title: 'After Works.',
-  titleTemplate: ''
+  title: "After Works.",
+  titleTemplate: ""
  },
  components: {
   MainVisual: MainVisual,
   ContentsLead: ContentsLead,
   ContentsTitle: ContentsTitle,
   CardList: CardList,
-  Contact: Contact
+  Contact: Contact,
+  AdsenseList: AdsenseList
  },
  data: function() {
   return {
-   response: ''
-  }
+   response: ""
+  };
  },
- async asyncData({$axios}) {
-  let url = '/json/page/index.json' + process.env.cashBuster
-  if(process.server) {
-   url = 'https://afterworks.jp' + url
-  }
-  else {
-   let hostname = location.hostname
-   if(hostname === 'localhost') {
-    hostname += ':3000'
+ async asyncData({ $axios }) {
+  let url = "/json/page/index.json" + process.env.cashBuster;
+  if (process.server) {
+   url = "https://afterworks.jp" + url;
+  } else {
+   let hostname = location.hostname;
+   if (hostname === "localhost") {
+    hostname += ":3000";
    }
-   url = location.protocol + '//' + hostname + url
+   url = location.protocol + "//" + hostname + url;
   }
 
-  const res = await $axios.get(url)
+  const res = await $axios.get(url);
   return {
    response: res.data
-  }
+  };
  },
  mounted: function() {
   /*------------------------------------------
    Scrooll Magic
   --------------------------------------------*/
-  let sMagicController = new ScrollMagic.Controller()
+  let sMagicController = new ScrollMagic.Controller();
 
-  let $sMagicFadeIn = document.querySelectorAll('.s-magic-fadein')
-  const sMagicFadeInLength = $sMagicFadeIn.length
+  let $sMagicFadeIn = document.querySelectorAll(".s-magic-fadein");
+  const sMagicFadeInLength = $sMagicFadeIn.length;
 
-  if(sMagicFadeInLength > 0) {
-   Array.prototype.forEach.call($sMagicFadeIn, function ($item, i) {
+  if (sMagicFadeInLength > 0) {
+   Array.prototype.forEach.call($sMagicFadeIn, function($item, i) {
     let sMagicFadeInScene = new ScrollMagic.Scene({
      // 対象要素
      triggerElement: $item,
-     triggerHook: 'onEnter',
+     triggerHook: "onEnter",
      reverse: false,
      offset: 150
-    }).addTo(sMagicController)
-    
+    }).addTo(sMagicController);
+
     // アニメーション開始時の処理
-    sMagicFadeInScene.on('enter', function() {
-     const delayTime = $sMagicFadeIn[i].dataset.pcIndex * 300
-     $sMagicFadeIn[i].style.transitionDelay = delayTime + 'ms'
+    sMagicFadeInScene.on("enter", function() {
+     const delayTime = $sMagicFadeIn[i].dataset.pcIndex * 300;
+     $sMagicFadeIn[i].style.transitionDelay = delayTime + "ms";
      setTimeout(function() {
-      $sMagicFadeIn[i].style.transitionDelay = ''
-     }, delayTime)
-     $sMagicFadeIn[i].classList.add('s-magic-on')
-    })
+      $sMagicFadeIn[i].style.transitionDelay = "";
+     }, delayTime);
+     $sMagicFadeIn[i].classList.add("s-magic-on");
+    });
 
     // アニメーション完了時の処理
-    sMagicFadeInScene.on('end', function() {
-     sMagicFadeInScene.destroy(true)
-    })
-   
-   })
-
+    sMagicFadeInScene.on("end", function() {
+     sMagicFadeInScene.destroy(true);
+    });
+   });
   }
  }
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
