53c62d12234175341968319d97262a2544caa29e
InumberX <35207269+InumberX@users.noreply.github.com>
2021-09-21 16:38:17 +0900

    パッケージを最新化

--- api/aws/dev/send_mail/index.php
+++ api/aws/dev/send_mail/index.php
@@ -27,49 +27,50 @@ $frm_data = null;
 
 // 入力情報が存在する場合
 if (isset($post_data['frmData'])) {
-    $frm_data = $post_data['frmData'];
+  $frm_data = $post_data['frmData'];
 }
 // 存在しない場合
 else {
-    $is_error = true;
+  $is_error = true;
 }
 
 // エラーがない場合
 if (!$is_error) {
-    // -----------------------------------------------
-    // メール送信
-    // -----------------------------------------------
-    $param = [
-        'authKey' => $auth_key,
-        'frmData' => json_encode($frm_data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
-    ];
+  // -----------------------------------------------
+  // メール送信
+  // -----------------------------------------------
+  $param = [
+    'authKey' => $auth_key,
+    'frmData' => json_encode($frm_data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
+  ];
 
-    $curl = curl_init();
+  $curl = curl_init();
 
-    curl_setopt($curl, CURLOPT_URL, $send_mail_url);
-    curl_setopt($curl, CURLOPT_POST, TRUE);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $param);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_URL, $send_mail_url);
+  curl_setopt($curl, CURLOPT_POST, TRUE);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $param);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_USERPWD, $basic_username . ':' . $basic_password);
 
-    $response = curl_exec($curl);
+  $response = curl_exec($curl);
 
-    $response_header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-    $response_header = substr($response, 0, $header_size);
-    $response_body = substr($response, $header_size);
-    $response_result = json_decode($response_body, true);
+  $response_header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+  $response_header = substr($response, 0, $header_size);
+  $response_body = substr($response, $header_size);
+  $response_result = json_decode($response_body, true);
 
-    curl_close($curl);
+  curl_close($curl);
 }
 
 // エラーがない場合
 if ($response_result['result']['actStatus'] === 'success' && !$is_error) {
-    $result->actStatus = 'success';
+  $result->actStatus = 'success';
 }
 // エラーがある場合
 else {
-    $result->actStatus = 'error';
+  $result->actStatus = 'error';
 }
 
 echo json_encode(compact('result'));
